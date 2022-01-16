d7e5a9dcbe4774a17e69789bbc418c0cd81a62bd
Robin Hwang <s60439rd@gmail.com>
2022-01-16 13:58:47 +0800

    c version

--- products.py
+++ products.py
@@ -1,6 +1,14 @@
-#兩個維度的清單
-#清單中的清單
+#
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格'in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+#
 while True:
 	name = input('請輸入商品名稱')
 	if name == "q":
@@ -11,7 +19,11 @@ print(products)
 
 products[0][0]
 
-with open("products.csv", "w", encoding = 'utf-8') as f:
+#印出所有購買紀錄
+for d in products:
+	print('買了' + d[0] +',花了' + d[1] + '元')
+#寫入檔案
+with open('products.csv', "w", encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
